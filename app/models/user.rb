f17f0f3dd61b612bca99fbc00dfd61f288bc2dc2
f-mio <snowy.landscape.24.oct@gmail.com>
2021-02-11 15:56:48 +0900

    rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-#  validates :email,    uniqueness: true, format: {with: /¥/} # @を含む
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: "半角アルファベットと半角数字の2種類を使用して、6文字以上で入力してください。"
-  with_options presence: true, length: {maximum: 5}, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "全角文字で入力してください"} do
+  #  validates :email,    uniqueness: true, format: {with: /¥/} # @を含む
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: '半角アルファベットと半角数字の2種類を使用して、6文字以上で入力してください。'
+  with_options presence: true, length: { maximum: 5 }, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字で入力してください' } do
     validates :first_name
     validates :last_name
   end
-  with_options presence: true, length: {maximum: 10}, format: {with: /\A[ァ-ヶ]+\z/, message: "全角カタカナで入力してください"} do
+  with_options presence: true, length: { maximum: 10 }, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナで入力してください' } do
     validates :first_name_reading
     validates :last_name_reading
   end
