063d7eea46cc6104ccc0303332b7cb92ae5e6a67
f-mio <snowy.landscape.24.oct@gmail.com>
2021-02-18 22:04:06 +0900

    showアクションの追加
    ・showアクションの追加
    ・route.rbの間違い修正。
    　　　resource → resources

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(1)
+  end
+
   private
 
   def item_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  resource :items, only: [:index, :new, :create]
-
   root to: 'items#index'
+
+  resources :items, only: [:index, :new, :create, :show]
+
 end
