c63e47f2663ba1c9713191d9bd08563bad9383c3
f-mio <snowy.landscape.24.oct@gmail.com>
2021-02-19 23:25:01 +0900

    コントローラにupdateアクションを定義・処理の記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,18 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user.id
+      redirect_to item_path(params[:id])
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
