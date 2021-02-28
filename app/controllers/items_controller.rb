d279112d0cda8999c17f248d9bfd76d64fa2dc36
f-mio <snowy.landscape.24.oct@gmail.com>
2021-02-28 19:31:06 +0900

    ユーザ認証で許可するメソッドを変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :validate_user, only: [:edit, :update, :destroy]
   before_action :validate_item_status, only: [:edit, :destroy]
