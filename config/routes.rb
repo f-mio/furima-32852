c1e0e47fb0df36acff27cdb8f618212b295cc979
f-mio <snowy.landscape.24.oct@gmail.com>
2021-03-02 12:35:48 +0900

    インデントの修正、不要なメソッドの削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
 
-resources :items do
-    resources :purchases, only: [:index, :new, :create ]
+  resources :items do
+    resources :purchases, only: [:new, :create ]
   end
 
 end
