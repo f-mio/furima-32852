dc696bd30124ffcc0e7e05efaeab21737523c44a
f-mio <snowy.landscape.24.oct@gmail.com>
2021-03-15 21:31:03 +0900

    nginx導入におけるunicorn設定の変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,7 +8,7 @@ worker_processes 1
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 # ポート番号を指定
-listen 3000
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 # エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
